7dccb21e2312d12d8458d2aa38fc7a5665f5df15
mlazzell17@gmail.com <MicheSi>
2021-01-12 12:50:23 -0700

    added nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/home';
+import Nav from './components/navbar';
 
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+
       <Switch>
         <Route exact path='/' component={Home}/>
       </Switch>
