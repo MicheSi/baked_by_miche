db4b14f8cf05c83f6e1942285539cc7d1dcf4151
mlazzell17@gmail.com <MicheSi>
2021-01-12 13:41:50 -0700

    cleaned up code in app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/home';
 import Nav from './components/navbar';
 import Contact from './components/contact';
-
-import './App.css';
 import OtherBakedGoods from './components/otherBakedGoods';
 import Macarons from './components/macarons';
 import Cookies from './components/cookies';
 
-
+import './App.css';
 
 function App() {
   return (
